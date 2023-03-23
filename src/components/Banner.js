700929c1e31e5b6520789955e85933a09d328f12
jaysilver-5 <udojoshua3@gmail.com>
2023-03-23 19:21:10 +0100

    My first commit to github, details on the about page still missing

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,68 @@
 import React from 'react';
 
+import Image from '../assets/avatar.svg';
+
+import Profile from '../assets/profile.png'
+import { FaGithub, FaYoutube, FaDribbble, FaLinkedin } from 'react-icons/fa';
+//type animation
+import { TypeAnimation } from 'react-type-animation';
+
+import { motion } from 'framer-motion';
+
+import { fadeIn } from '../variants'
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+  <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+        {/*  text */}       
+        <div className='flex-1 text-center font-secondary lg:text-left'>
+          <motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount:0.7}} className='text-[55px] font-bold leading-[0.8] lg:text-[90px]'>
+            Joshua <span>Udo-Udo</span>
+          </motion.h1>
+          <motion.div variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount:0.7}} className='mb-6 text-[36px] lg:text-[60px] font-semibold uppercase loading-[1]'>
+            <span className='text-white mr-4'>I'm a</span>
+            < TypeAnimation sequence={[
+              'Developer',
+              2000,
+              'Designer',
+              2000
+            ]}
+              speed={50}
+              className='text-accent'
+              wrapper='span'
+              repeat={Infinity}
+            />
+          </motion.div>
+          <motion.p variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount:0.7}} className='mb-8 lg:mx-0 max-w-lg mx-auto lg:max-0'>
+            I am a full-stack developer and engineer with years of experience dedicated to delivering the best user experiences.
+          </motion.p>
+          <motion.div variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount:0.6}} className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            <button className='btn btn-lg'>Contact me</button>
+            <a href='#' className='text-gradient btn-link'>My portfolio</a>
+          </motion.div>
+          {/* socials */}
+          <motion.div variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount:0.7}} className='flex text-[20px] gap-x-6 max-w-mas mx-auto lg:mx-0'>
+            <a href='#'>
+              <FaYoutube />
+            </a>
+            <a href='#'>
+              <FaGithub />
+            </a>
+            <a href='#'>
+              <FaLinkedin />
+            </a>
+          </motion.div>
+        </div> 
+      {/* image */}
+      <motion.div variants={fadeIn('down', 0.5)} initial="hidden" whileInView={'show'} className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto'>
+        <img src={Profile} alt="" />
+      </motion.div>
+      </div>
+    </div>
+  </section>
+  )
 };
 
 export default Banner;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,85 @@
 import React from 'react';
+//icon
+import { BsArrowUpRight } from 'react-icons/bs';
+//motion
+import {motion} from 'framer-motion';
+//variants
+import { fadeIn } from '../variants';
+
+//services data
+const services = [
+  {
+    name:"Full-stack development",
+    description: 
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel molestiae quas vel.",
+    link: "Learn more."
+},
+{  name:"Mobile development",
+  description: 
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel molestiae quas vel.",
+  link: "Learn more."
+},
+{  name:"Digital marketing",
+  description: 
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia molestiae quas vel molestiae quas vel.",
+  link: "Learn more."
+}
+]
 
 const Services = () => {
-  return <div>Services</div>;
+  return(
+     <section className='section' id='services'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row'>
+          {/* text */}
+          <motion.div 
+          variants={fadeIn('right', 0.3)}
+           initial='hidden'
+           whileInView={"show"}
+           viewport={{once: false, amount: 0.3}}
+          className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0'>
+            <h2 className='h2 text-accent mb-6'>What I do</h2>
+            <h3 className="h3 max-w-[455px] mb-16">
+              I'm a Full-stack Developer with years of experience.
+            </h3>
+            <button className='btn btn-sm'>See my work</button>
+          </motion.div>
+          {/* services */}
+          <motion.div  
+          variants={fadeIn('left', 0.5)}
+           initial='hidden'
+           whileInView={"show"}
+           viewport={{once: false, amount: 0.3}}
+          className='flex-1 '>
+            {/* service list */}
+            <div>
+            
+              {services.map((service, index) => {
+                //destructure services
+                const {name, description, link} = service;
+                console.log(name)
+
+                return(
+                  <div className='border-b border-white/20 h-[146px] mb-[38px] flex' key={index}>
+                    <div className='max-w-[476px]'>
+                      <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>{name}</h4>
+                      <p className='font-secondary leading-tight'>{description}</p>
+                    </div>
+                    <div className='flex flex-col flex-1 items-end'>
+                      <a href='#' className='btn w-9 h-9 mb-[42px] flex items-center justify-center text-sm'>
+                        <BsArrowUpRight />
+                      </a>
+                      <a href='#' className='text-gradient'>{link}</a>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </motion.div>
+        </div>
+      </div>
+     </section>
+  )
 };
 
 export default Services;
