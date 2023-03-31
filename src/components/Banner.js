094103b498dcd3ef2d4ae07ded142879a47b7d88
Joshua Udo <73034334+jaysilver-5@users.noreply.github.com>
2023-03-31 19:54:10 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,7 +15,7 @@ import { fadeIn } from '../variants'
 
 const Banner = () => {
   return (
-  <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+  <section className='min-h-[85vh] text-white lg:min-h-[78vh] flex items-center' id='home'>
     <div className='container mx-auto'>
       <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
         {/*  text */}       
